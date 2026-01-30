c75570b65d8fc40a824e61f3f2012e7a390e115b
gayathri260306 <gayathri31141@gmail.com>
2026-01-30 18:10:39 +0530

    Solved loop problem

---
+++ Python/Loops.py
@@ -0,0 +1,9 @@
+# Problem: Loops
+# Difficulty: Easy
+'''The provided code stub reads an integer,n , from STDIN. For all non-negative integers i<n , print i^2.''''
+
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(0,n):
+        print(i**2)
