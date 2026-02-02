6572c8b2573ccfd36eb07cb665b5a4adb94cb569
gayathri260306 <gayathri31141@gmail.com>
2026-02-02 21:48:12 +0530

    Added japanese city attribute query

---
+++ MySql/Japanese Cities' Attributes.sql
@@ -0,0 +1,5 @@
+-- Problem: Japanese Cities' Attributes
+-- Difficulty: Easy
+--Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
+SELECT * from city 
+where countrycode='JPN';
