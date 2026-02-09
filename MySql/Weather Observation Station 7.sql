76fe96eb57e7ba77574e4a443065970a026febb9
gayathri260306 <gayathri31141@gmail.com>
2026-02-09 21:50:46 +0530

    Added select city ending with vowels

---
+++ MySql/Weather Observation Station 7.sql
@@ -0,0 +1,6 @@
+-- Problem: Weather Observation Station 7
+-- Difficulty: Easy
+--Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+
+select distinct city from station
+where city regexp '[aeiouAEIOU]$';
